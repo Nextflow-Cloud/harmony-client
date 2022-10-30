2881e35086c2a8510f5408e1a286b15843c2ae11
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-10-30 18:55:26 +0000

    fix: modal theming

--- src/components/ModalDialog.tsx
+++ src/components/ModalDialog.tsx
@@ -1,3 +1,7 @@
+import { useRef } from "preact/hooks";
+import { shallowEqual } from "react-redux";
+import { useAppSelector } from "../utilities/redux/redux";
+
 interface Button {
     text: string;
     id: string;
@@ -13,9 +17,13 @@ interface Props {
 }
 
 const ModalDialog = ({ title, content, onClose, buttons }: Props) => {
+
+    const dialog = useRef<HTMLDivElement>(null);
+    const darkTheme = useAppSelector(state => state.preferences.theme === "dark", shallowEqual);
+    
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-slate-400 bg-opacity-50 backdrop-blur-sm" onContextMenu={e => e.preventDefault()}>
-            <div className="rounded-md bg-white p-5 drop-shadow-lg opacity-100">
+        <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 backdrop-blur-sm z-50 fadein" onContextMenu={e => e.preventDefault()} ref={dialog}>
+            <div className={`rounded-md ${darkTheme ? "bg-gray-900 text-gray-200" : "bg-white"} p-5 drop-shadow-lg opacity-100 max-w-prose`}>
                 <div className="flex flex-col space-y-4">
                     <div className="flex flex-row items-center justify-between">
                         <h2 className="text-2xl font-semibold">
@@ -27,7 +35,14 @@ const ModalDialog = ({ title, content, onClose, buttons }: Props) => {
                     </div>
                     <div className="flex flex-row justify-end">
                         {buttons.map(button => (
-                            <button key={button.id} className={`px-4 py-2 mx-2 rounded-md ${button.primary ? "bg-green-500 hover:bg-green-600" : "bg-gray-300  hover:bg-gray-400"}`} onClick={() => onClose(button.id)}>
+                            <button 
+                                key={button.id} 
+                                className={`px-4 py-2 mx-2 rounded-md cursor-default ${button.primary ? "bg-green-500 hover:bg-green-600" : (darkTheme ? "bg-gray-700 hover:bg-gray-600" : "bg-gray-300  hover:bg-gray-400")}`} 
+                                onClick={() => {
+                                    onClose(button.id);
+                                    dialog.current?.classList.add("fadeout");
+                                }}
+                            >
                                 {button.text}
                             </button>
                         ))}

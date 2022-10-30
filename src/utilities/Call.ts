ea3cf88b98f3cc5bc63bc4abe4e0901bd2dfb135
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-10-30 18:58:03 +0000

    refactor: call

--- src/utilities/Call.ts
+++ src/utilities/Call.ts
@@ -1,7 +1,7 @@
 import ExtendedWebSocket, { WebSocketCodes } from "./ExtendedWebSocket";
 import { Device } from "mediasoup-client";
-import { RtpCapabilities } from "mediasoup-client/lib/RtpParameters";
-import { Transport, TransportOptions } from "mediasoup-client/lib/Transport";
+import { MediaKind, RtpCapabilities, RtpParameters } from "mediasoup-client/lib/RtpParameters";
+import { IceCandidate, Transport, TransportOptions } from "mediasoup-client/lib/Transport";
 import { ConsumerOptions } from "mediasoup-client/lib/Consumer";
 import { Producer } from "mediasoup-client/lib/Producer";
 import { StateUpdater } from "preact/hooks";
@@ -61,34 +61,54 @@ class Call {
         
     }
     protected async onNewProducer(id: string, type: "audio" | "video") {
-        const transportData = await this.socket.request({ type: WebSocketCodes.TRANSPORT, data: { id: this.id } });
-        const transportOptions = (transportData.data as Record<string, unknown>).transport as TransportOptions;
+        const transportData = (await this.socket.request({ type: WebSocketCodes.TRANSPORT, data: { channel_id: this.id, producer: false } })).data!;
+        (transportData.ice_candidates as any) = (transportData.ice_candidates as any).map((c: { ip: any; }) => {
+            c.ip = c.ip.V4.join(".");
+            return c;
+        });
+        const transportOptions = {
+            dtlsParameters: transportData.dtls_parameters,
+            iceCandidates: transportData.ice_candidates,
+            iceParameters: transportData.ice_parameters,
+            sctpParameters: transportData.sctp_parameters,
+            id: transportData.id
+        } as TransportOptions;
         const transport = this.device.createRecvTransport(transportOptions);
         transport.on("connect", ({ dtlsParameters }, callback, errback) => {
-            this.socket.request({ type: WebSocketCodes.DTLS, data: { dtlsParameters, transportId: transportOptions.id, id: this.id } })
+            this.socket.request({ type: WebSocketCodes.DTLS, data: { dtls_parameters: dtlsParameters, transport_id: transportOptions.id, channel_id: this.id } })
                 .then(() => callback())
                 .catch(e => errback(e));
         });
         const consumerData = (await this.socket.request({ type: WebSocketCodes.CONSUME, data: { 
-            rtpCapabilities: this.device.rtpCapabilities, 
-            producerId: id, 
-            id: this.id, 
-            transportId: (transportData.data!.transport as { id: string; }).id 
+            rtp_capabilities: this.device.rtpCapabilities, 
+            producer_id: id, 
+            channel_id: this.id, 
+            transport_id: transportOptions.id 
         } })).data as {
-            consumer: ConsumerOptions;
-            userId: string;
-            uniqueId: string;
+            // consumer: ConsumerOptions;
+            // user_id: string;
+            // unique_id: string;
+            id: string;
+            kind: MediaKind;
+            rtp_parameters: RtpParameters;
+            producer_id: string;
+            producer_paused: boolean;
         };
-        const consumer = await transport.consume(consumerData.consumer);
-        await this.socket.request({ type: WebSocketCodes.RESUME, data: { id: this.id, consumerId: consumer.id } });
+        const consumer = await transport.consume({
+            rtpParameters: consumerData.rtp_parameters,
+            id: consumerData.id,
+            kind: consumerData.kind,
+            producerId: consumerData.producer_id
+        });
+        await this.socket.request({ type: WebSocketCodes.RESUME, data: { channel_id: this.id, consumer_id: consumer.id } });
         const stream = new MediaStream([consumer.track]);
         console.log(`Received new stream of type ${type}`);
         if (type === "audio") {
             this.audioTracks.push({
                 consumerId: consumer.id,
                 stream,
-                userId: consumerData.userId,
-                uniqueId: consumerData.uniqueId
+                userId: "consumerData.user_id",
+                uniqueId: "consumerData.unique_id"
             });
             const audio = document.getElementById("audio") as HTMLAudioElement;
             if (!audio.srcObject)
@@ -100,8 +120,8 @@ class Call {
             this.videoTracks.push({
                 consumerId: consumer.id,
                 stream,
-                userId: consumerData.userId,
-                uniqueId: consumerData.uniqueId
+                userId:" consumerData.user_id",
+                uniqueId:" consumerData.unique_id"
             });
             this.videoListStateUpdater(this.videoTracks);
             // const videoList = document.getElementById("video-list") as HTMLDivElement;
@@ -115,33 +135,49 @@ class Call {
         }
     }
     async connect() {
-        const joinCall = (await this.socket.request({ type: WebSocketCodes.CAPABILITIES, data: { id: this.id } })).data as { 
-            rtpCapabilities: RtpCapabilities; 
-            callMembers: { userId: string; uniqueId: string; }[]; 
+        const joinCall = (await this.socket.request({ type: WebSocketCodes.CAPABILITIES, data: { channel_id: this.id } })).data as { 
+            rtp_capabilities: RtpCapabilities; 
+            call_members: { user_id: string; unique_id: string; producers: { id: string; kind: MediaKind; rtp_parameters: RtpParameters }[] }[]; 
         };
-        this.callMembers = new Map(joinCall.callMembers.map(m => [m.uniqueId, m.userId]));
+        this.callMembers = new Map(joinCall.call_members.map(m => [m.unique_id, m.user_id]));
         this.memberStateUpdater(Array.from(this.callMembers.entries()));
-        await this.device.load({ routerRtpCapabilities: joinCall.rtpCapabilities as RtpCapabilities });
+        await this.device.load({ routerRtpCapabilities: joinCall.rtp_capabilities as RtpCapabilities });
+        joinCall.call_members.forEach(m => {
+            m.producers.forEach(p => {
+                this.onNewProducer(p.id, p.kind);
+            });
+        });
     }
     async produce(track: MediaStreamTrack): Promise<[Transport, Producer]> {
-        const transportData = await this.socket.request({ type: WebSocketCodes.TRANSPORT, data: { id: this.id, produce: true } });
-        const transportOptions = (transportData.data as Record<string, unknown>).transport as TransportOptions;
+        const transportRequest = await this.socket.request({ type: WebSocketCodes.TRANSPORT, data: { channel_id: this.id, producer: true } })
+        const transportData = transportRequest.data as Record<string, unknown>;
+        (transportData.ice_candidates as any) = (transportData.ice_candidates as any).map((c: { ip: any; }) => {
+            c.ip = c.ip.V4.join(".");
+            return c;
+        });
+        const transportOptions = {
+            dtlsParameters: transportData.dtls_parameters,
+            iceCandidates: transportData.ice_candidates,
+            iceParameters: transportData.ice_parameters,
+            sctpParameters: transportData.sctp_parameters,
+            id: transportData.id
+        } as TransportOptions;
         const transport = this.device.createSendTransport(transportOptions);
         transport.on("connect", ({ dtlsParameters }, callback, errback) => {
-            this.socket.request({ type: WebSocketCodes.DTLS, data: { dtlsParameters, id: this.id, transportId: transportOptions.id } })
+            this.socket.request({ type: WebSocketCodes.DTLS, data: { dtls_parameters: dtlsParameters, channel_id: this.id, transport_id: transportOptions.id } })
                 .then(() => callback())
                 .catch(e => errback(e));
         });
         transport.on("produce", (parameters, callback, errback) => {
-            this.socket.request({ type: WebSocketCodes.PRODUCE, data: { parameters, id: this.id, transportId: transportOptions.id } })
-                .then(data => callback(data.data!.producer as { id: string; }))
+            this.socket.request({ type: WebSocketCodes.PRODUCE, data: { kind: parameters.kind, rtp_parameters: parameters.rtpParameters, channel_id: this.id, transport_id: transportOptions.id } })
+                .then(data => callback(data.data as { id: string; }))
                 .catch(e => errback(e));
         });
         const producer = await transport.produce({ track, encodings: [{ maxBitrate: 1000000 }] });
         return [transport, producer];
     }
     async hangUp() {
-        await this.socket.request({ type: WebSocketCodes.LEAVE_CALL, data: { id: this.id } });
+        await this.socket.request({ type: WebSocketCodes.LEAVE_CALL, data: { channel_id: this.id } });
         this.audioTracks = [];
         this.videoTracks = [];
         this.callMembers = new Map();

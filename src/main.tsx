4d0bfa78ab43e11ecb562067e297afcfcab5eab5
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2023-01-29 13:01:19 -0600

    feat: warning

--- src/main.tsx
+++ src/main.tsx
@@ -11,4 +11,14 @@ Object.defineProperty(window, "internals", {
     configurable: false
 });
 
+const warn = () => console.error("%cAttention!\n%cPasting arbitrary code here could allow your authentication token and therefore your account to be stolen by a malicious attacker. This type of attack is called Self-XSS. Please make sure you know what you are doing. Do not execute code that you do not understand.\n%cThis is not an error and is intended behaviour. Learn more at https://nextflow.cloud/developers/self-xss", "font-size: 48px; color: red;", "font-size: 24px; color: black;", "font-size: 18px;");
+
+setTimeout(() => warn(), 10000);
+
+setInterval(() => {
+    // Warn the user about Self-XSS
+    warn();
+}, 120000);
+
+
 render(<App />, document.getElementById("app") as HTMLElement);
